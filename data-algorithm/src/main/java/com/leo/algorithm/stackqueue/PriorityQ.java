e0061f3ba577b0d69db16b247bc532a3721a6d1b
lzlqq <390650599@qq.com>
2019-02-04 23:13:17 +0800

    stackqueue

---
+++ data-algorithm/src/main/java/com/leo/algorithm/stackqueue/PriorityQ.java
@@ -0,0 +1,61 @@
+package com.leo.algorithm.stackqueue;
+
+/**
+ * Created by LSH7120 on 2019/2/1.
+ */
+public class PriorityQ {
+    private int maxSize;
+    private long[] queArray;
+    private int nItems;
+
+    public PriorityQ(int maxSize) {
+        this.maxSize = maxSize;
+        queArray = new long[maxSize];
+        nItems = 0;
+    }
+
+    public void insert(long item) {
+        int j;
+        if (nItems == 0) {
+            queArray[nItems++] = item;
+        } else {
+            for (j = nItems - 1; j >= 0; j--) {
+                if (item > queArray[j]) {
+                    queArray[j + 1] = queArray[j];
+                } else {
+                    break;
+                }
+            }
+            queArray[j + 1] = item;
+            nItems++;
+        }
+    }
+
+    public long remove() {
+        return queArray[--nItems];
+    }
+
+    public long peekMin() {
+        return queArray[nItems - 1];
+    }
+
+    public boolean isEmpty() {
+        return nItems == 0;
+    }
+
+    public boolean isFull() {
+        return nItems == maxSize;
+    }
+
+    public static void main(String[] args) {
+        PriorityQ thePQ = new PriorityQ(5);
+        thePQ.insert(30);
+        thePQ.insert(50);
+        thePQ.insert(40);
+        thePQ.insert(10);
+
+        while (!thePQ.isEmpty()) {
+            System.out.print(thePQ.remove() + " ");
+        }
+    }
+}
