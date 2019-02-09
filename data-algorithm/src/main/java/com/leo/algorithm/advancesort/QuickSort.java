52c01b5dd7998fdb0e6126fec73b8b0da0e91f63
lzlqq <390650599@qq.com>
2019-02-09 23:08:24 +0800

    sort

---
+++ data-algorithm/src/main/java/com/leo/algorithm/advancesort/QuickSort.java
@@ -0,0 +1,100 @@
+package com.leo.algorithm.advancesort;
+
+/**
+ * 快速排序 N*logN
+ */
+public class QuickSort {
+    static class ArrayIns {
+        private long[] theArray;
+        private int nElems;
+
+        public ArrayIns(int max) {
+            theArray = new long[max];
+            nElems = 0;
+        }
+
+        public void insert(long value) {
+            theArray[nElems] = value;
+            nElems++;
+        }
+
+        public void display() {
+            System.out.print("A=");
+            for (int i = 0; i < nElems; i++) {
+                System.out.print(theArray[i] + " ");
+            }
+            System.out.println();
+        }
+
+        public void quickSort() {
+            recQuickSort(0, nElems - 1);
+        }
+
+        private void recQuickSort(int left, int right) {
+            int size = right - left + 1;
+            if (size < 10) {
+                insertionSort(left, right);
+            } else {
+                long median = medianOf3(left, right);
+                int partition = partitionIt(left, right, median);
+                recQuickSort(left, partition - 1);
+                recQuickSort(partition + 1, right);
+            }
+        }
+
+        private int partitionIt(int left, int right, long pivot) {
+            int leftPtr = left;
+            int rightPtr = right - 1;
+            while (true) {
+                while (theArray[++leftPtr] < pivot) {
+                    ;
+                }
+                while (theArray[--rightPtr] > pivot) {
+                    ;
+                }
+                if (leftPtr >= rightPtr) {
+                    break;
+                } else {
+                    swap(leftPtr, rightPtr);
+                }
+            }
+            swap(leftPtr, right - 1);
+            return leftPtr;
+        }
+
+        private long medianOf3(int left, int right) {
+            int center = (left + right) / 2;
+            if (theArray[left] > theArray[center]) {
+                swap(left, center);
+            }
+
+            if (theArray[left] > theArray[right]) {
+                swap(left, right);
+            }
+            if (theArray[center] > theArray[right]) {
+                swap(center, right);
+            }
+            swap(center, right - 1);
+            return theArray[right - 1];
+        }
+
+        private void swap(int dex1, int dex2) {
+            long temp = theArray[dex1];
+            theArray[dex1] = theArray[dex2];
+            theArray[dex2] = temp;
+        }
+
+        private void insertionSort(int left, int right) {
+            int in, out;
+            for (out = left + 1; out <= right; out++) {
+                long temp = theArray[out];
+                in = out;
+                while (in > left && theArray[in - 1] >= temp) {
+                    theArray[in] = theArray[in - 1];
+                    in--;
+                }
+                theArray[in] = temp;
+            }
+        }
+    }
+}
---
+++ data-algorithm/src/main/java/com/leo/algorithm/advancesort/ShellSort.java
@@ -0,0 +1,61 @@
+package com.leo.algorithm.advancesort;
+
+/**
+ * 希尔排序
+ */
+public class ShellSort {
+
+    public static void main(String[] args) {
+        ArraySh arr = new ArraySh(10);
+        for (int i = 0; i < 10; i++) {
+            arr.insert((long) (Math.random() * 100));
+        }
+        arr.display();
+        arr.shellSort();
+        arr.display();
+    }
+
+    static class ArraySh {
+        private long[] theArray;
+        private int nElems;
+
+        public ArraySh(int max) {
+            theArray = new long[max];
+            nElems = 0;
+        }
+
+        public void insert(long value) {
+            theArray[nElems] = value;
+            nElems++;
+        }
+
+        public void display() {
+            System.out.print("A=");
+            for (int i = 0; i < nElems; i++) {
+                System.out.print(theArray[i] + " ");
+            }
+            System.out.println();
+        }
+
+        public void shellSort() {
+            int inner, outer;
+            long temp;
+            int h = 1;
+            while (h < nElems / 3) {
+                h = h * 3 + 1;
+            }
+            while (h > 0) {
+                for (outer = h; outer < nElems; outer++) {
+                    temp = theArray[outer];
+                    inner = outer;
+                    while (inner > h - 1 && theArray[inner - h] >= temp) {
+                        theArray[inner] = theArray[inner - h];
+                        inner -= h;
+                    }
+                    theArray[inner] = temp;
+                }
+                h = (h - 1) / 3;
+            }
+        }
+    }
+}
